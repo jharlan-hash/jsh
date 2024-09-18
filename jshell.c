ec532bb1a0e30c106d2f5edcd13c1f955d250cd7
Jack Sovern <25jaso@decorah.school>
2024-09-17 19:12:26 -0500

    add exit command & print working directory

--- jshell.c
+++ jshell.c
@@ -2,20 +2,42 @@
 #include <string.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <limits.h>
 
+/*returns nothing*/
+void builtin_cd(char *dest){
+    if (dest != NULL){
+        // checking if cd is called with no arguments
+        chdir(dest);
+    } else {
+        // checking user's environment variables for "$HOME"
+        chdir(getenv("HOME"));
+    }
+}
+
+/*returns a pointer to a buffer containing the command entered and returns 0 on error*/
 char *jsh_getline(){
     char *buf;
+    int glret;
     size_t size = 512;
 
     buf = (char*)malloc(sizeof(char[size]));
+
     if (!buf){
         perror("Error allocating to buf");
     }
-    getline(&buf, &size, stdin);
 
-    return buf;
+    glret = getline(&buf, &size, stdin);
+
+    if (!glret){
+        perror("Error getting line in jsh_getline");
+        return 0;
+    } else {
+        return buf;
+    }
 }
 
+/*returns a pointer to an array of commands & arguments*/
 char **jsh_splitline(char *line){
     char **arg_array;
     char *token;
@@ -55,16 +77,12 @@ int jsh_execute(char **args){
         return 1;
     }
 
+
+    /*checks whether the command is cd*/
     if (!strcmp(args[0], "cd")){
-        if (args[1] != NULL){
-            chdir(args[1]);
-        } else {
-            chdir(getenv("HOME"));
-        }
-#ifdef DEBUG
-        printf("%s", args[1]);
-        system("pwd");
-#endif
+        builtin_cd(args[1]);
+    } else if (!strcmp(args[0], "exit")){
+        return 1;
     } else {
         pid = fork();
         if (pid == 0){
@@ -89,10 +107,12 @@ int jsh_execute(char **args){
 void jsh_loop(void){
     char *line;
     char **args;
+    char cwd[PATH_MAX];
     int status;
 
     do {
-        printf("$ ");
+        getcwd(cwd, sizeof(cwd));
+        printf("%s\n$ ", cwd);
         line = jsh_getline();
         args = jsh_splitline(line);
         status = jsh_execute(args);
